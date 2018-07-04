020f27ff3b39a95646a3012add7c3b9c827b5043
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-04 14:32:50 +0200

    //wakeel update

--- TestGit2/TestGit2/Controllers/HomeController.cs
+++ TestGit2/TestGit2/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace TestGit2.Controllers
 {
     public class HomeController : Controller
     {
+        //wakeel update
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
