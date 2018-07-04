fd494b648d775f097390b36999382cb61e967acc
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-04 14:10:17 +0200

    Add Wakeel2Controller

---
+++ TestGit2/TestGit2/Controllers/Wakeel2Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGit2.Controllers
+{
+    public class Wakeel2Controller : Controller
+    {
+        //
+        // GET: /Wakeel2/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
