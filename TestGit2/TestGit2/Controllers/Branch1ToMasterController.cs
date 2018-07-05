1e2a41456bf13245301b988511ecd16fc04700db
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-05 08:58:56 +0200

    test

---
+++ TestGit2/TestGit2/Controllers/Branch1ToMasterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGit2.Controllers
+{
+    public class Branch1ToMasterController : Controller
+    {
+        //
+        // GET: /Branch1ToMaster/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
