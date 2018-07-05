4e95e277279c1d2ed9748b06db0e900add3349ad
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-05 09:03:25 +0200

    Add Branch2ToMasterController.cs

---
+++ TestGit2/TestGit2/Controllers/Branch2ToMasterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGit2.Controllers
+{
+    public class Branch2ToMasterController : Controller
+    {
+        //
+        // GET: /Branch2ToMaster/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
