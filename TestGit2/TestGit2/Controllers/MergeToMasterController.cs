7c5da55f55659ae8b90b07cd44df28027872b5da
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-05 08:19:55 +0200

    Add Controller MergeToMaster

---
+++ TestGit2/TestGit2/Controllers/MergeToMasterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGit2.Controllers
+{
+    public class MergeToMasterController : Controller
+    {
+        //
+        // GET: /MergeToMaster/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
